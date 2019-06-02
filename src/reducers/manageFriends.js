dad7ec9f409574fd8fed73490789ee974f964544
Yevgeniy <yevgeniyerot@gmail.com>
2019-06-01 21:55:40 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -10,9 +10,13 @@ export function manageFriends(state = {
     case "REMOVE_FRIEND":
       const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
 
-      return ({...state.friends.slice(0, removalIndex),
-        ...state.friends.slice(removalIndex + 1)}
-      );
+      return ({
+        ...state,
+        friends: [
+          ...state.friends.slice(0, removalIndex),
+          ...state.friends.slice(removalIndex + 1)
+          ]
+      });
     default:
       return state
   }
